fe259c80d4f74bb6323b47b312a52018c4ef2171
toanvo <toan.vo@doctoranywhere.com>
2024-03-08 10:55:03 +0700

    Comment from github

--- Multiplication.java
+++ Multiplication.java
@@ -1,6 +1,7 @@
 public class Multiplication implements MathOperation {
     @Override
     public double calculate(double left, double right) {
+        // Comment from github
         return left * right;
     }
 }
