a8439fa57a6ec84d5f3936030c0faf5b1792d04c
hugobuta <vongoctoan@gmail.com>
2024-03-08 14:28:19 +0700

    A change from hugobuta

--- Addition.java
+++ Addition.java
@@ -1,7 +1,7 @@
 public class Addition implements MathOperation {
     @Override
     public double calculate(double left, double right) {
-        // To perform an addition operation
+        // To perform an addition operation - change from hugobuta
         return left + right;
     }
 }
