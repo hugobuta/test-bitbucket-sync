59166859876e6466a9d7336dddf96d833ef03bdc
toanvo <toan.vo@doctoranywhere.com>
2024-03-08 10:31:49 +0700

    Add the comment to addition

--- Addition.java
+++ Addition.java
@@ -1,6 +1,7 @@
 public class Addition implements MathOperation {
     @Override
     public double calculate(double left, double right) {
+        // To perform an addition operation
         return left + right;
     }
 }
