bf0d28b60c1216e731080170ae060cdcf4bb77e4
toanvo <toan.vo@doctoranywhere.com>
2024-03-08 10:36:04 +0700

    Yet another comment

--- Subtraction.java
+++ Subtraction.java
@@ -1,6 +1,7 @@
 public class Subtraction implements MathOperation {
     @Override
     public double calculate(double left, double right) {
+        // Yet another comment
         return left - right;
     }
 }
